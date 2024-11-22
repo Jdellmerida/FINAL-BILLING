4c77e9d07a56f25cb19ec3a2a3ee660b62a29529
Jdellmerida <145127363+Jdellmerida@users.noreply.github.com>
2024-11-22 12:16:45 +0800

    FINALS BILLING
    payment receipt at design front end nalang kulang

---
+++ BILLING/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.USERNAME = New System.Windows.Forms.TextBox()
+        Me.PASSWORD = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.EXITBTN = New System.Windows.Forms.Button()
+        Me.LOGINBTN = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'USERNAME
+        '
+        Me.USERNAME.Location = New System.Drawing.Point(308, 127)
+        Me.USERNAME.Name = "USERNAME"
+        Me.USERNAME.Size = New System.Drawing.Size(156, 20)
+        Me.USERNAME.TabIndex = 0
+        '
+        'PASSWORD
+        '
+        Me.PASSWORD.Location = New System.Drawing.Point(308, 182)
+        Me.PASSWORD.Name = "PASSWORD"
+        Me.PASSWORD.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.PASSWORD.Size = New System.Drawing.Size(156, 20)
+        Me.PASSWORD.TabIndex = 1
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(308, 208)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(126, 17)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "SHOW PASSWORD"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'EXITBTN
+        '
+        Me.EXITBTN.Location = New System.Drawing.Point(308, 238)
+        Me.EXITBTN.Name = "EXITBTN"
+        Me.EXITBTN.Size = New System.Drawing.Size(75, 23)
+        Me.EXITBTN.TabIndex = 3
+        Me.EXITBTN.Text = "EXIT"
+        Me.EXITBTN.UseVisualStyleBackColor = True
+        '
+        'LOGINBTN
+        '
+        Me.LOGINBTN.Location = New System.Drawing.Point(389, 238)
+        Me.LOGINBTN.Name = "LOGINBTN"
+        Me.LOGINBTN.Size = New System.Drawing.Size(75, 23)
+        Me.LOGINBTN.TabIndex = 4
+        Me.LOGINBTN.Text = "LOGIN"
+        Me.LOGINBTN.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(234, 130)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "USERNAME"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(234, 185)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "PASSWORD"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(68, 50)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(592, 24)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "NEW PARANG DAILY BILLING SYSTEM USING RFID READER"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(759, 386)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LOGINBTN)
+        Me.Controls.Add(Me.EXITBTN)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.PASSWORD)
+        Me.Controls.Add(Me.USERNAME)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents USERNAME As TextBox
+    Friend WithEvents PASSWORD As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents EXITBTN As Button
+    Friend WithEvents LOGINBTN As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
